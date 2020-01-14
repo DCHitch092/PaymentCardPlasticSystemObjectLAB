83acd56a275b444362508ef539d207375df93ca2
L3n1ad <danieltoth0910@gmail.com>
2020-01-14 14:13:58 +0000

    DONE PaymentCardPlasticSystemObject

---
+++ src/main/java/PaymenCardPlasticSystemObject.java
@@ -0,0 +1,23 @@
+public abstract class PaymenCardPlasticSystemObject {
+    String cardNumber;
+    String expiryDate;
+    int securityNumber;
+
+    public PaymenCardPlasticSystemObject(String cardNumber, String expiryDate, int securityNumber) {
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.securityNumber = securityNumber;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public int getSecurityNumber() {
+        return securityNumber;
+    }
+}
